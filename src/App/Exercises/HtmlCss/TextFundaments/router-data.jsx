045b968cd4c7a6046267979181bc50c13a348c7f
Kamil <kcybulski93@gmail.com>
2023-08-29 18:59:02 +0200

    TextFundaments

---
+++ src/App/Exercises/HtmlCss/TextFundaments/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { TextFundaments } from './TextFundaments';
+
+export const TextFundamentsMetaData = {
+  path: 'text-fundaments-css',
+  date: '29-08-2023',
+  linkLabel: 'Praca z tekstem',
+  blockNo: 1,
+  element: <TextFundaments />,
+  tags: ['attribute', 'tekst', 'css'],
+};
