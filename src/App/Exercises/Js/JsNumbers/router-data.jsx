13cc48dbc0352694826b6153753c87cfe142b114
kcybulski93 <kcybulski93@gmail.com>
2023-09-09 18:17:00 +0200

    Basic numbers operators

---
+++ src/App/Exercises/Js/JsNumbers/router-data.jsx
@@ -0,0 +1,8 @@
+import { JsNumbers } from './JsNumbers';
+
+export const JsNumbersRouterMetaData = {
+  path: 'js-numbers',
+  date: '07-09-2023',
+  linkLabel: 'Liczby w JS',
+  element: <JsNumbers />,
+};
