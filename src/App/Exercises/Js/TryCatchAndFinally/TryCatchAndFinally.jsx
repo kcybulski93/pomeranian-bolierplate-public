b7e246fc6dc8afb7eefe835641d37e96f1756831
kcybulski93 <kcybulski93@gmail.com>
2023-10-03 05:11:41 +0200

    Try Catch and Finally

--- src/App/Exercises/Js/TryCatchAndFinally/TryCatchAndFinally.jsx
+++ src/App/Exercises/Js/TryCatchAndFinally/TryCatchAndFinally.jsx
@@ -1,1 +1,25 @@
-export const TryCatchAndFinally = () => {};
+import { useEffect } from 'react';
+
+export const TryCatchAndFinally = () => {
+  function dumb() {
+    const randomNumber = Math.round(Math.random());
+
+    if (randomNumber) {
+      console.log('Wszystko działa');
+    } else {
+      throw new Error('Niestety nie działa');
+    }
+  }
+
+  useEffect(() => {
+    try {
+      dumb();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log('wszystko się już zakończyło');
+    }
+  }, []);
+
+  return <span>Hello Try Catch</span>;
+};
